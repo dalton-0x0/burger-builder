85ac1082c9f272fc743d69308462b1ad4bb308ff
Dalton C. Okechukwu <chike911@yahoo.com>
2020-07-30 16:03:34 -0600

    updated layout and toolbar for navigation items

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import BurgerLogo from "../../assets/images/burger-logo.png";
+import classes from "./Logo.css";
+
+const Logo = (props) => {
+  return (
+    <div className={classes.Logo}>
+      <img src={BurgerLogo} alt="Logo for Burger" />
+    </div>
+  );
+};
+
+export default Logo;
