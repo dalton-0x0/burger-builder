eaf1754a6084ba88b04a7c648d57816fc7765baf
Dalton C. Okechukwu <chike911@yahoo.com>
2020-07-31 14:22:00 -0600

    added sidedrawer, backdrop and navigation for mobile responsiveness

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,17 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./NavigationItems.css";
-import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
-const NavigationItems = (props) => {
-  return (
+const navigationItems = () => (
     <ul className={classes.NavigationItems}>
-      <NavigationItem link="/" active>
-        Burger Builder
-      </NavigationItem>
-      <NavigationItem link="/">Checkout</NavigationItem>
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
-  );
-};
+);
 
-export default NavigationItems;
+export default navigationItems;
