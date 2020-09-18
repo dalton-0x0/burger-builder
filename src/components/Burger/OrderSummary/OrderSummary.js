c0166a6578bff523d9b10b80471ddaa8ff8cb53a
Dalton C. Okechukwu <chike911@yahoo.com>
2020-09-18 16:49:22 -0600

    unsafe componentwillupdate lifecycle methods

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,7 +4,7 @@ import Aux from "../../../hoc/Aux/Aux";
 import Button from "../../UI/Button/Button";
 
 class OrderSummary extends Component {
-  componentWillUpdate() {
+  UNSAFE_componentWillUpdate() {
     console.log("[OrderSummary] WillUpdate");
   }
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,7 @@ class Modal extends Component {
     return nextProps.show !== this.props.show;
   }
 
-  componentWillUpdate() {
+  UNSAFE_componentWillUpdate() {
     console.log("[Modal] WillUpdate");
   }
 
