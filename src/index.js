d0f1e5c2be5209107ef0b618a68e7c67ed9396b8
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-02-19 13:41:14 -0700

    installed redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 const app = (
     <Provider store={store}>
