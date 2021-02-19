c8691dd594c5a4152c329751048a04a5aa69bf68
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-02-19 14:54:12 -0700

    applying middleware to createStore

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import reducer from "./store/reducer";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(
+    burgerBuilderReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 
 const app = (
     <Provider store={store}>
