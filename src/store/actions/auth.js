8fa1a1b0b9a5a5fcde3804d38125d7e8aab6c6c1
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-02-26 11:13:33 -0700

    initial setup getting signup token from firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -23,5 +24,23 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return (dispatch) => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true,
+        };
+        axios
+            .post(
+                "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoXTxCTd4R6n5rLg8gREAQ13DUYY1MmR0",
+                authData
+            )
+            .then((response) => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch((err) => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
