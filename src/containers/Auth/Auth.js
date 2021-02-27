d2be41d20d39c6d72f3feccde8966ba61dfcc195
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-02-27 13:47:42 -0700

    switching auth mode

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -91,8 +91,15 @@ class Auth extends Component {
         event.preventDefault();
         this.props.onAuth(
             this.state.controls.email.value,
-            this.state.controls.password.value
+            this.state.controls.password.value,
+            this.state.isSignup
         );
+    };
+
+    switchAuthModeHandler = () => {
+        this.setState((prevState) => {
+            return { isSignup: !prevState.isSignup };
+        });
     };
 
     render() {
@@ -125,6 +132,9 @@ class Auth extends Component {
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
+                <Button clicked={this.switchAuthModeHandler} btnType="Danger">
+                    SWITCH TO {this.state.isSignup ? "SIGNIN" : "SIGNUP"}
+                </Button>
             </div>
         );
     }
@@ -132,7 +142,8 @@ class Auth extends Component {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password)),
+        onAuth: (email, password, isSignup) =>
+            dispatch(actions.auth(email, password, isSignup)),
     };
 };
 
