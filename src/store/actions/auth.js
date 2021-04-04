750b8978e49f9bb271b69f83c9442f02fe0f1964
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-04-04 00:41:39 -0600

    sagas replacing auth actions with side effects

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -45,38 +44,11 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 export const auth = (email, password, isSignup) => {
-    return (dispatch) => {
-        dispatch(authStart());
-        
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true,
-        };
-        
-        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoXTxCTd4R6n5rLg8gREAQ13DUYY1MmR0";
-                       
-        if (!isSignup) {
-            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDoXTxCTd4R6n5rLg8gREAQ13DUYY1MmR0";
-        }
-        
-        axios
-            .post(url, authData)
-            .then((response) => {
-                const expirationDate = new Date(
-                    new Date().getTime() + response.data.expiresIn * 1000
-                );
-                localStorage.setItem("token", response.data.idToken);
-                localStorage.setItem("expirationDate", expirationDate);
-                localStorage.setItem("userId", response.data.localId);
-                dispatch(
-                    authSuccess(response.data.idToken, response.data.localId)
-                );
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch((err) => {
-                dispatch(authFail(err.response.data.error));
-            });
+    return {
+        type: actionTypes.AUTH_USER,
+        email: email,
+        password: password,
+        isSignup: isSignup,
     };
 };
 
@@ -88,25 +60,7 @@ export const setAuthRedirectPath = (path) => {
 };
 
 export const authCheckState = () => {
-    return (dispatch) => {
-        const token = localStorage.getItem("token");
-        if (!token) {
-            dispatch(logout());
-        } else {
-            const expirationDate = new Date(
-                localStorage.getItem("expirationDate")
-            );
-            if (expirationDate <= new Date()) {
-                dispatch(logout());
-            } else {
-                const userId = localStorage.getItem("userId");
-                dispatch(authSuccess(token, userId));
-                dispatch(
-                    checkAuthTimeout(
-                        (expirationDate.getTime() - new Date().getTime()) / 1000
-                    )
-                );
-            }
-        }
-    };
+    return {
+        type: actionTypes.AUTH_CHECK_STATE
+      };
 };
