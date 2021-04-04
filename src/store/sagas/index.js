5b20ad505f696ecc2643a848e14d982ba45019ae
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-04-04 00:42:44 -0600

    sagas replacing auth actions with side effects

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,8 +1,13 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, all } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionTypes";
-import { logoutSaga } from "./auth";
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from "./auth";
 
 export function* watchAuth() {
-    yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga),
+    ]);
 }
