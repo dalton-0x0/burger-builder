da7255ab0932c1727e34649d0a9fecac6f6dfbcf
Dalton C. Okechukwu [personal] <chike911@yahoo.com>
2021-04-08 18:37:33 -0600

    added watchOrder saga to index

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,8 +1,14 @@
-import { takeEvery, all } from "redux-saga/effects";
+import { takeEvery, all, takeLatest } from "redux-saga/effects";
 
 import * as actionTypes from "../actions/actionTypes";
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from "./auth";
+import {
+    logoutSaga,
+    checkAuthTimeoutSaga,
+    authUserSaga,
+    authCheckStateSaga,
+} from "./auth";
 import { initIngredientsSaga } from "./burgerBuilder";
+import { purchaseBurgerSaga, fetchOrdersSaga } from "./order";
 
 export function* watchAuth() {
     yield all([
@@ -15,4 +21,9 @@ export function* watchAuth() {
 
 export function* watchBurgerBuilder() {
     yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
+}
+
+export function* watchOrder() {
+    yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+    yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
